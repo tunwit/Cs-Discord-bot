b12f2583421c54c48a0341fe785843528a61c2b7
tunwit <tunwit2458@gmail.com>
2024-07-08 18:47:01 +0700

    ability to track edited message

--- cogs/messagetrack.py
+++ cogs/messagetrack.py
@@ -55,7 +55,7 @@ class messageAPI(commands.Cog):
             attach = len(payload.cached_message.attachments)
         embed=discord.Embed(description=f"Message sent by <@{author}> deleted in <#{channel}>",color=0x795ae8)
         embed.add_field(name="Content",value=f"` {content} `")
-        embed.add_field(name="Attachments",value=f"` {attach} `")
+        embed.add_field(name="📫 Attachments",value=f"` {attach} `")
         if author == 123:
             author = self.bot.user
         else:
@@ -68,34 +68,36 @@ class messageAPI(commands.Cog):
     
     @commands.Cog.listener()
     async def on_raw_message_edit(self,payload:discord.RawMessageUpdateEvent):
-        print(paylaod)
-        # with open('database\\data.json','r') as database:
-        #         data = json.load(database) 
-        # data = data["messagetrack"]["channel"]
-        # if not data:
-        #     return
-        # if str(payload.guild_id) not in list(data):
-        #     return
-        # author = 123
-        # channel = payload.channel_id
-        # content = "Unknown"
-        # attach = "N/A"
-        # if payload.cached_message:
-        #     author = payload.cached_message.author.id
-        #     content = payload.cached_message.content
-        #     attach = len(payload.cached_message.attachments)
-        # embed=discord.Embed(description=f"Message sent by <@{author}> deleted in <#{channel}>",color=0x795ae8)
-        # embed.add_field(name="Content",value=f"` {content} `")
-        # embed.add_field(name="Attachments",value=f"` {attach} `")
-        # if author == 123:
-        #     author = self.bot.user
-        # else:
-        #     author = payload.cached_message.author 
-        # embed.set_author(name=author.name+author.discriminator,icon_url=author.display_avatar.url)
-        # embed.set_footer(text = time.strftime("%D | %H:%M:%S"))  
-        # guild:discord.Guild = self.bot.get_guild(payload.guild_id)
-        # channel = guild.get_channel(data[str(payload.guild_id)])
-        # await  channel.send(embed=embed)
+        with open('database\\data.json','r') as database:
+                data = json.load(database) 
+        data = data["messagetrack"]["channel"]
+        if not data:
+            return
+        if str(payload.guild_id) not in list(data):
+            return
+        author = int(payload.data["author"]["id"])
+        channel = payload.data["channel_id"]
+        before = "Unknown"
+        attach_ori = "N/A"
+        if payload.cached_message:
+            author = payload.cached_message.author.id
+            before = payload.cached_message.content
+            attach_ori = len(payload.cached_message.attachments)
+
+        embed=discord.Embed(description=f"Message edited by <@{author}> in <#{channel}>",color=0x795ae8)
+        embed.add_field(name="Orinal",value=f"` {before} `")
+        embed.add_field(name="Edited",value=f"` {payload.data['content']} `",inline=True)
+        embed.add_field(name="📫 Original Attachments",value=f"` {attach_ori} `",inline=False)
+        embed.add_field(name="📫 Edited Attachments",value=f"` {len(payload.data['attachments'])} `",inline=True)
+
+        guild = self.bot.get_guild(payload.guild_id)
+        member = guild.get_member(author)
+        print(member)
+        embed.set_author(name=member.name+member.discriminator,icon_url=member.display_avatar.url)
+        embed.set_footer(text = time.strftime("%D | %H:%M:%S"))  
+        guild:discord.Guild = self.bot.get_guild(payload.guild_id)
+        channel = guild.get_channel(data[str(payload.guild_id)])
+        await  channel.send(embed=embed)
 
 async def setup(bot):    
   await bot.add_cog(messageAPI(bot))   
