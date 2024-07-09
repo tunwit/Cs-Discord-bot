f63693093096e6142a29c11c6f277500ce1f8063
tunwit <tunwit2458@gmail.com>
2024-07-09 21:29:37 +0700

    add linkshortenner feature

--- cogs/linkshortener.py
+++ cogs/linkshortener.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import requests
+import re
 
 class linkshortener(commands.Cog):
     def __init__(self, bot ):
@@ -11,12 +12,25 @@ class linkshortener(commands.Cog):
     @app_commands.describe(url="link or url")
     async def linkshortener(self,interaction:discord.Interaction,url:str):
         await interaction.response.defer()
+        regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        if not re.match(regex, url):
+            embed=discord.Embed(description=f"This is not valid url",color=0xcc8c2d)
+            embed.set_author(name=interaction.user.name,icon_url=interaction.user.display_avatar.url)
+            await interaction.followup.send(embed = embed,ephemeral=True)
+            return
         result = requests.post('https://littleshort.vercel.app/api/link',json={"origin":url})
         if result.status_code == 201:
             await interaction.followup.send(f'https://littleshort.vercel.app/{result.json()["data"]["uniqueID"]}')
         else:
-            await interaction.followup.send(f'There is an error occured status code {result.status_code}')
-
+            embed=discord.Embed(description=f"There is an error occured status code {result.status_code}",color=0xcc8c2d)
+            embed.set_author(name=interaction.user.name,icon_url=interaction.user.display_avatar.url)
+            await interaction.followup.send(embed = embed,ephemeral=True)
 
 async def setup(bot):    
   await bot.add_cog(linkshortener(bot))  
