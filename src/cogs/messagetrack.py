5bc16fcfafb2d44dcb57fbc71d88c0208f73182c
tunwit <tunwit2458@gmail.com>
2024-09-15 17:45:07 +0700

    Use mongodb on message track feature

--- src/cogs/messagetrack.py
+++ src/cogs/messagetrack.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import time
-import json
 from discord.app_commands import Choice
 
 class messageAPI(commands.Cog):
@@ -14,36 +13,28 @@ class messageAPI(commands.Cog):
   Choice(name = "ON",value="ON"),
   Choice(name = "OFF",value="OFF"),])
     async def message(self,interaction:discord.Interaction,channel:discord.TextChannel,status:str): 
-        await interaction.response.defer()
-        with open('database/data.json','r') as database:
-            data = json.load(database) 
+        database = self.bot.cs_mango["messagetrack"]
+        data = database.find_one({"guild_id":str(interaction.guild.id)})
         if status == 'ON':
-            if str(interaction.guild.id) in list(data["messagetrack"]["channel"]):
-                data["messagetrack"]["channel"].pop(str(interaction.guild.id))
-            data["messagetrack"]["channel"].update({
-                interaction.guild.id:channel.id
-            })
-
-            with open('database/data.json', 'w') as database:
-                json.dump(data, database,indent=4)
-            await interaction.followup.send(f"Set <#{channel.id}> to Track-message",ephemeral=True)
+            if not data:  
+                database.insert_one({
+                "guild_id":str(interaction.guild.id),
+                "text_channel":str(channel.id)
+                })                            
+            await interaction.response.send_message(f"Set <#{channel.id}> to Track-message",ephemeral=True)
         else:
-            if str(interaction.guild.id) in list(data["messagetrack"]["channel"]):
-                if channel.id == data["messagetrack"]["channel"][str(interaction.guild.id)]:
-                    data["messagetrack"]["channel"].pop(str(interaction.guild.id))
-
-            with open('database/data.json', 'w') as database:
-                json.dump(data, database,indent=4)
-            await interaction.followup.send(f"<#{channel.id}> No longer Track-message",ephemeral=True)
+            if data:
+                database.delete_one({"guild_id":str(interaction.guild.id)})
+            else:
+                await interaction.response.send_message(f"<#{channel.id}> Is not on the list",ephemeral=True)
+                return
+            await interaction.response.send_message(f"<#{channel.id}> No longer Track-message",ephemeral=True)
 
     @commands.Cog.listener()
     async def on_raw_message_delete(self,payload:discord.RawMessageDeleteEvent):
-        with open('database/data.json','r') as database:
-                data = json.load(database) 
-        data = data["messagetrack"]["channel"]
+        database = self.bot.cs_mango["messagetrack"]
+        data = database.find_one({"guild_id":str(payload.guild_id)})
         if not data:
-            return
-        if str(payload.guild_id) not in list(data):
             return
         author = 123
         if payload.cached_message:
@@ -53,34 +44,28 @@ class messageAPI(commands.Cog):
         channel = payload.channel_id
         content = "Unknown"
         attach = "N/A"
-        author = 123
+        author = "Unknown"
         if payload.cached_message:
             content = payload.cached_message.content
             attach = len(payload.cached_message.attachments)
-
+            author = payload.cached_message.author.id
         try:
             embed=discord.Embed(description=f"Message sent by <@{author}> deleted in <#{channel}>",color=0x795ae8)
             embed.add_field(name="Content",value=f"` {content} `")
             embed.add_field(name="📫 Attachments",value=f"` {attach} `")
-            if author == 123:
-                author = self.bot.user
-            else:
-                author = payload.cached_message.author 
-            embed.set_author(name=author.name,icon_url=author.display_avatar.url)
             embed.set_footer(text = time.strftime("%D | %H:%M:%S"))  
             guild:discord.Guild = self.bot.get_guild(payload.guild_id)
-            channel = guild.get_channel(data[str(payload.guild_id)])
+            channel = guild.get_channel(int(data["text_channel"]))
             await  channel.send(embed=embed)
-        except:pass
+        except Exception as e:
+            print(e)
+    
     
     @commands.Cog.listener()
     async def on_raw_message_edit(self,payload:discord.RawMessageUpdateEvent):
-        with open('database/data.json','r') as database:
-                data = json.load(database) 
-        data = data["messagetrack"]["channel"]
+        database = self.bot.cs_mango["messagetrack"]
+        data = database.find_one({"guild_id":str(payload.guild_id)})
         if not data:
-            return
-        if str(payload.guild_id) not in list(data):
             return
         if payload.cached_message:
             user = payload.cached_message.author
@@ -101,15 +86,14 @@ class messageAPI(commands.Cog):
             embed.add_field(name="Edited",value=f"` {payload.data['content']} `",inline=True)
             embed.add_field(name="📫 Original Attachments",value=f"` {attach_ori} `",inline=False)
             embed.add_field(name="📫 Edited Attachments",value=f"` {len(payload.data['attachments'])} `",inline=True)
-
             guild = self.bot.get_guild(payload.guild_id)
-            member = guild.get_member(author)
-            embed.set_author(name=member.name,icon_url=member.display_avatar.url)
             embed.set_footer(text = time.strftime("%D | %H:%M:%S"))  
             guild:discord.Guild = self.bot.get_guild(payload.guild_id)
-            channel = guild.get_channel(data[str(payload.guild_id)])
+            channel = guild.get_channel(int(data["text_channel"]))
             await  channel.send(embed=embed)
-        except:pass
+        except Exception as e:
+            print(e)
+
 
 async def setup(bot):    
   await bot.add_cog(messageAPI(bot))   
